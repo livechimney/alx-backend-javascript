8c0c0b20adb99805002f90a685c2566a47df3fd1
livechimney <adrianmuriuki1366@gmail.com>
2024-10-03 18:29:23 +0000

    change

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -8,5 +8,10 @@ import ClassRoom from './0-classroom';
  * @returns An array of {@link ClassRoom}s.
  */
 export default function initializeRooms() {
-    return [19, 20, 34].map((size) => new ClassRoom(size));
+    return [
+	    19, 
+	    20, 
+	    34 
+   
+    ].map((size) => new ClassRoom(size));
 }
